998b98ef8cd6f1d76aa8befb58ccd0531ec46260
John L <johnny@synergetx.io>
2024-03-15 19:06:48 -0700

    added auth

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+import { Amplify } from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import config from './aws-exports';
 import reportWebVitals from './reportWebVitals';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
